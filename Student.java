2dcd3a186dbefd007e2730be118465ff613cb81e
Wyan <pixelartforever@gmail.com>
2017-12-12 14:34:57 -0600

    Initial commit

---
+++ Student.java
@@ -0,0 +1,163 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Student here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Student extends Actor
+{
+    int studentNumber;
+    int gradeLevel;
+    int itDelay;
+    
+    double average;
+    boolean it;
+    
+    String firstName;
+    String lastName;
+    String upKey;
+    String downKey;
+    String leftKey;
+    String rightKey;
+    
+    public Student(int number, String first, String last, int grade, double avg, boolean isIt)
+    {
+        studentNumber = number;
+        firstName = first;
+        lastName = last;
+        gradeLevel = grade;
+        average = avg;
+        it = isIt;
+        
+        if (studentNumber == 1)
+        {
+            upKey = "w";
+            downKey = "s";
+            leftKey = "a";
+            rightKey = "d";
+        }
+        else if (studentNumber == 2)
+        {
+            upKey = "u";
+            downKey = "j";
+            leftKey = "h";
+            rightKey = "k";
+        }
+        else
+        {
+            upKey = "up";
+            downKey = "down";
+            leftKey = "left";
+            rightKey = "right";
+        }
+        
+        itDelay = 200;
+        if ( it == true)
+        {
+            setImage("ppl1.png");
+        }
+        
+    }
+    
+    public String getfirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+    
+    public int getGradeLevel()
+    {
+        return gradeLevel;
+    }
+    
+    public double getAverage()
+    {
+        return average;
+    }
+    
+    public boolean getIt()
+    {
+        return it;
+    }
+    
+    public String toString()
+    {
+        return String.format("Name: %11s,%8s\nGrade: %19d\nAverage: %15.1f %%\n",lastName, firstName, gradeLevel, average);
+    }
+    
+    private void checkCollision()
+    {
+        Actor otherStudent = getOneIntersectingObject(Student.class);
+        
+        if( otherStudent !=null)
+        {
+            
+           if ( itDelay >= 200)
+           {
+               
+               if ( it == true )
+               {
+                   it = false;
+                   setImage("ppl3.png");
+                   itDelay = 0;
+               }
+               else
+               {
+                   it = true;
+                   setImage("ppl1.png");
+                   itDelay = 0;
+               }
+               
+           }
+           
+        }
+        
+    }
+    
+    private void movement()
+    {
+        
+        if ( Greenfoot.isKeyDown(upKey) )
+        {
+            setLocation( getX(), getY() - 3);
+        }
+        else if ( Greenfoot.isKeyDown(downKey) )
+        {
+            setLocation( getX(), getY() + 3);
+        }
+        else if ( Greenfoot.isKeyDown(leftKey) )
+        {
+            setLocation( getX() - 3, getY());
+        }
+        else if ( Greenfoot.isKeyDown(rightKey) )
+        {
+            setLocation( getX() + 3, getY());
+        }
+        
+    }
+    
+    /**
+     * Act - do whatever the Student wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        checkCollision();
+        movement();
+        
+        if( Greenfoot.mouseClicked(this) )
+        {   
+            System.out.println( toString() );
+        }
+        
+        itDelay ++;
+    }  
+    
+}
